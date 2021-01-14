27b8ecba0d65b270f795f6a3e7cbcd7a3440cdbb
Qaiser Mehmood <qmdev@creativesims.com>
2021-01-15 04:09:54 +0500

    Add project files.

---
+++ BlazorFilestackApplication/PolicyOptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFilestackApplication
+{
+    public static class CallOptions
+    {
+        public const string Pick = "pick";
+        public const string Read = "read";
+        public const string Stat = "stat";
+        public const string Write = "write";
+        public const string WriteUrl = "writeUrl";
+        public const string Store = "store";
+        public const string Convert = "convert";
+        public const string Remove = "remove";
+        public const string Exif = "exif";
+        public const string RunWorkflow = "runWorkflow";
+    }
+
+    public class PolicyOptions
+    {
+        public List<string> Call;
+        public int Expiry;
+        public string Handle;
+        public string Url;
+        public string Path;
+        public string Container;
+        public int MinSize;
+        public int MaxSize;
+    }
+}
---
+++ BlazorFilestackApplication/wwwroot/Operations.js
@@ -0,0 +1,11 @@
+﻿var openFilePicker = function (apiKey, policy, signature) {
+    const clientOptions = {
+        security: {
+            policy: policy,
+            signature: signature
+        }
+    }
+
+    const client = filestack.init(apiKey, clientOptions);
+    client.picker().open();
+}
